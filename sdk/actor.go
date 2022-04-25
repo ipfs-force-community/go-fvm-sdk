79c9e82cc07ce436261ff76c78225b8e3dc1ca2a
hunjixin <1084400399@qq.com>
2022-04-25 18:10:14 +0800

    add sdk for actor

--- sdk/actor.go
+++ sdk/actor.go
@@ -1,85 +1,45 @@
 package sdk
 
 import (
-	addr "github.com/filecoin-project/go-address"
+	address "github.com/filecoin-project/go-address"
 	"github.com/ipfs-force-community/go-fvm-sdk/sdk/sys"
 	"github.com/ipfs-force-community/go-fvm-sdk/sdk/types"
+	"github.com/ipfs/go-cid"
 )
 
 /// Resolves the ID address of an actor. Returns `None` if the address cannot be resolved.
 /// Successfully resolving an address doesn't necessarily mean the actor exists (e.g., if the
 /// addresss was already an actor ID).
-func ResolveAddress(addr addr.Address) (types.ActorId, error) {
+func ResolveAddress(addr address.Address) (types.ActorId, error) {
 	return sys.ResolveAddress(addr)
 }
 
-/*
-
 /// Look up the code ID at an actor address. Returns `None` if the actor cannot be found.
-pub fn get_actor_code_cid(addr: &Address) -> Option<Cid> {
-    let bytes = addr.to_bytes();
-    let mut buf = [0u8; MAX_CID_LEN];
-    unsafe {
-        let ok = sys::actor::get_actor_code_cid(
-            bytes.as_ptr(),
-            bytes.len() as u32,
-            buf.as_mut_ptr(),
-            MAX_CID_LEN as u32,
-        )
-        // Can only fail due to memory corruption
-        .expect("failed to lookup actor code cid");
-        if ok == 0 {
-            // Cid::read_bytes won't read until the end, just the bytes it needs.
-            Some(Cid::read_bytes(&buf[..]).expect("invalid cid returned"))
-        } else {
-            None
-        }
-    }
+func GetActorCodeCid(addr address.Address) (*cid.Cid, error) {
+	return sys.GetActorCodeCid(addr)
 }
 
 /// Generates a new actor address for an actor deployed
 /// by the calling actor.
-pub fn new_actor_address() -> Address {
-    let mut buf = [0u8; MAX_ACTOR_ADDR_LEN];
-    unsafe {
-        let len = sys::actor::new_actor_address(buf.as_mut_ptr(), MAX_ACTOR_ADDR_LEN as u32)
-            .expect("failed to create a new actor address");
-        Address::from_bytes(&buf[..len as usize]).expect("syscall returned invalid address")
-    }
+func NewActorAddress() (address.Address, error) {
+	return sys.NewActorAddress()
 }
 
 /// Creates a new actor of the specified type in the state tree, under
 /// the provided address.
 /// TODO this syscall will change to calculate the address internally.
-pub fn create_actor(actor_id: ActorID, code_cid: &Cid) -> SyscallResult<()> {
-    let cid = code_cid.to_bytes();
-    unsafe { sys::actor::create_actor(actor_id, cid.as_ptr()) }
+func CreateActor(actorId types.ActorId, codeCid cid.Cid) error {
+	return sys.CreateActor(actorId, codeCid)
 }
 
 /// Determines whether the supplied CodeCID belongs to a built-in actor type,
 /// and to which.
-pub fn resolve_builtin_actor_type(code_cid: &Cid) -> Option<actor::builtin::Type> {
-    let cid = code_cid.to_bytes();
-    unsafe {
-        let res = sys::actor::resolve_builtin_actor_type(cid.as_ptr())
-            .expect("failed to determine if CID belongs to builtin actor");
-        // The zero value represents "unknown" and is not modelled in the enum,
-        // so it'll be converted to a None.
-        FromPrimitive::from_i32(res)
-    }
+func ResolveBuiltinActorType(codeCid cid.Cid) (types.ActorType, error) {
+	return sys.ResolveBuiltinActorType(codeCid)
 }
 
 /// Returns the CodeCID for a built-in actor type. Aborts with IllegalArgument
 /// if the supplied type is invalid.
-pub fn get_code_cid_for_type(typ: Type) -> Cid {
-    let mut buf = [0u8; MAX_CID_LEN];
-    unsafe {
-        let len =
-            sys::actor::get_code_cid_for_type(typ as i32, buf.as_mut_ptr(), MAX_CID_LEN as u32)
-                .expect("failed to get CodeCID for type");
-        Cid::read_bytes(&buf[..len as usize]).expect("invalid cid returned")
-    }
+func GetCodeCidForType(actorT types.ActorType) (cid.Cid, error) {
+	return sys.GetCodeCidForType(actorT)
 }
-
-
-*/
