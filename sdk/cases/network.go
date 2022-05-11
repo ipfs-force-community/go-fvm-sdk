bd058e939f0fb6ab73a9a45eae771232717b7258
一页素书 <2931107265@qq.com>
2022-05-11 17:52:00 +0800

    feat: test sdk network&gas case

---
+++ sdk/cases/network.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/ipfs-force-community/go-fvm-sdk/sdk"
+	"github.com/ipfs-force-community/go-fvm-sdk/sdk/testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func main() {}
+
+//go:export invoke
+func Invoke(_ uint32) uint32 {
+	t := testing.NewTestingT()
+	defer t.CheckResult()
+
+	epoch, err := sdk.CurrEpoch()
+	assert.Nil(t, err)
+	assert.Equal(t, 0, int(epoch))
+
+	ver, err := sdk.Version()
+	assert.Nil(t, err)
+	assert.Equal(t, 15, int(ver))
+
+	fee, err := sdk.BaseFee()
+	assert.Nil(t, err)
+	assert.Equal(t, "100", fee.Big().String())
+
+	value, err := sdk.TotalFilCircSupply()
+	assert.Nil(t, err)
+	assert.Equal(t, "2000000000000000000000000000", value.Big().String())
+	return 0
+}
