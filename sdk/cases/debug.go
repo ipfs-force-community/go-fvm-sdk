936f2c6135a2a319bdc342a2212bc394390a1552
一页素书 <2931107265@qq.com>
2022-05-12 13:15:51 +0800

    feat: syscall test case for debug&send

--- sdk/cases/debug.go
+++ sdk/cases/debug.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/ipfs-force-community/go-fvm-sdk/sdk"
+	"github.com/ipfs-force-community/go-fvm-sdk/sdk/testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func main() {}
+
+//go:export invoke
+func Invoke(_ uint32) uint32 {
+	t := testing.NewTestingT()
+	defer t.CheckResult()
+
+	logger, err := sdk.NewLogger()
+	assert.Nil(t, err, "create debug logger %v", err)
+
+	enabled := logger.Enabled()
+	assert.Equal(t, false, enabled)
+
+	err = logger.Log("")
+	assert.Nil(t, err)
+
+	return 0
+}
