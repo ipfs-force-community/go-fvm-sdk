b4dff89e01a615dadc25b9e3e17a147baa600c8e
zl <zl03jsj@163.com>
2022-07-06 15:33:17 +0800

    go-fvm-sdk-tool support go v1.18

--- tools/sdk_tool/src/utils.rs
+++ tools/sdk_tool/src/utils.rs
@@ -45,15 +45,13 @@ pub fn check_go_install() -> Result<bool> {
         Ok(child) => {
             let output = child.wait_with_output()?;
             let version_str = String::from_utf8(output.stdout)?;
-            if version_str.contains("go1.16.") || version_str.contains("go1.17.") {
-                Ok(true)
-            } else {
-                Err(anyhow!(
-                    "uncorect go version must be go 1.16.x/go1.17.x but got {}",
-                    version_str
-                ))
+            for s in SUPPORT_GO_VERSIONS.iter() {
+                if version_str.contains(s) {
+                    return Ok(true);
+                }
             }
-        }
+            Err(anyhow!("incorrect go version:{},must in {:?}", version_str, SUPPORT_GO_VERSIONS))
+        },
         Err(e) => {
             if let ErrorKind::NotFound = e.kind() {
                 Err(anyhow!("unable to found go-fvm-sdk-tools(fvm), please install this tool in https://go.dev/dl"))
@@ -63,3 +61,5 @@ pub fn check_go_install() -> Result<bool> {
         }
     }
 }
+
+const SUPPORT_GO_VERSIONS: &'static [&'static str] = &["go1.16.", "go1.17.", "go1.18."];
