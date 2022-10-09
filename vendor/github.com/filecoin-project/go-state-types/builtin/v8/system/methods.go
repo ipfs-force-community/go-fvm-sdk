cf46610ae3a0cd3354082071271e6d3fef6fa26f
simon yi <letmtwb@gmail.com>
2022-10-09 15:06:06 +0800

    新增context支持

--- vendor/github.com/filecoin-project/go-state-types/builtin/v8/system/methods.go
+++
@@ -1,9 +0,0 @@
-package system
-
-import (
-	"github.com/filecoin-project/go-state-types/abi"
-)
-
-var Methods = []interface{}{
-	1: *new(func(interface{}, *abi.EmptyValue) *abi.EmptyValue), // Constructor
-}
